64b93f4c6a201da396ac702601b55224f6139f70
Dave Compton <davecompton7@gmail.com>
2016-04-19 03:42:12 +0000

    displaying a checkerboard

--- Main.hs
+++ Main.hs
@@ -1,25 +1,28 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE OverloadedLists       #-}
-
-module Main where
-
-import           Data.FileEmbed
 import           Data.Map       (Map)
 import qualified Data.Map as M
 import           Reflex.Dom
 
 main :: IO ()
--- main = mainWidgetWithCss $(embedFile "style.css") atompit
 main = mainWidget atompit
 
 atompit :: MonadWidget t m => m ()
 atompit = do
   let ns = Just "http://www.w3.org/2000/svg"
+
   let svgAttrs :: Map String String
-      svgAttrs = [ ("version", "1.1")
+      svgAttrs = M.fromList [ ("version", "1.1")
                  , ("baseProfile", "full")
                  , ("width", "300px")
-                 , ("height", "200px")
-                 , ("viewBox", "0 0 300 200") ]
+                 , ("height", "300px")
+                 , ("viewBox", "0 0 8 8") ]
+
+      showChecker :: MonadWidget t m => Int -> Int -> m ()
+      showChecker row col = 
+          elWith "rect" (ElConfig ns (M.fromList [("x", show row), ("y", show col), ("width", "1"), ("height", "1"), ("fill", if (row + col) `mod` 2 == 0 then "blue" else "grey")])) $ return ()
+
+      checkers = [0..7] >>= \r -> 
+                 [0..7] >>= \c -> 
+                 [showChecker r c]
+
   elWith "svg" (ElConfig ns svgAttrs) $ 
-    elWith "rect" (ElConfig ns (M.fromList [("width", "100%"), ("height", "100%"), ("fill", "red")])) (return ())
+    sequence_ checkers
