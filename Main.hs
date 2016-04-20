5786ab3e663376b9749bf202c114f13265d0e1ea
Dave Compton <davecompton7@gmail.com>
2016-04-20 05:07:21 +0000

    introduced Action type

--- Main.hs
+++ Main.hs
@@ -3,20 +3,24 @@ import qualified Data.Map as M
 import           Reflex.Dom
 import           Data.Monoid
 
+type Cell = (Int, Int)
+
+data Action = Tick Int | SetStart Cell
+
 main :: IO ()
 main = mainWidget view
 
 ns = Just "http://www.w3.org/2000/svg"
 
+-- view :: MonadWidget t m => m (Event t Action)
 view :: MonadWidget t m => m ()
 view = do
     let 
-        showChecker :: MonadWidget t m => Int -> Int -> m (Event t ())
+        showChecker :: MonadWidget t m => Int -> Int -> m (Event t Action)
         showChecker r c = do
-            (el, ev) <- elDynAttrNS' ns "rect" (constDyn $ "x" =: show c <> "y" =: show r <> "width" =: "1" <> "height" =: "1" <> "fill" =: if (r + c) `mod` 2 == 0 then "blue" else "grey") $ return ()
-            return $ domEvent Click el
+            (el, _) <- elDynAttrNS' ns "rect" (constDyn $ "x" =: show c <> "y" =: show r <> "width" =: "1" <> "height" =: "1" <> "fill" =: if (r + c) `mod` 2 == 0 then "blue" else "grey") $ return ()
+            return $ const (SetStart (r,c)) <$> domEvent Click el -- tricky
 
-        showCheckers :: MonadWidget t m => m [Event t ()]
         showCheckers = 
             let ckrs = [0..7] >>= \r -> 
                        [0..7] >>= \c -> 
@@ -25,6 +29,6 @@ view = do
     
     
     elDynAttrNS' ns "svg" (constDyn $ "viewBox" =: "0 0 8 8" <>  "width" =: "500" <> "height" =: "500") $ 
-        elStopPropagationNS ns "g" Click $ fmap leftmost showCheckers
+        elStopPropagationNS ns "g" Click (fmap leftmost showCheckers)
     return ()
 
