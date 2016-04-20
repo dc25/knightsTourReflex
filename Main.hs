dc874abac11884430523cd1916e0ce637716e257
Dave Compton <davecompton7@gmail.com>
2016-04-20 06:26:39 +0000

    introduced update function and cleaned up formatting

--- Main.hs
+++ Main.hs
@@ -1,7 +1,9 @@
+{-# LANGUAGE RecursiveDo #-}
+
 import           Data.Map       (Map)
 import qualified Data.Map as M
 import           Reflex.Dom
-import           Data.Monoid
+import           Data.Monoid ((<>))
 
 type Cell = (Int, Int)
 
@@ -10,7 +12,7 @@ data Model = Model { rows :: Int
                    , path :: [Cell]
                    , board :: [Cell]
                    }
-init rc cc = 
+initModel rc cc = 
     let board = do r <- [0..rc-1] 
                    c <- [0..cc-1] 
                    return (r,c)
@@ -19,17 +21,34 @@ init rc cc =
 data Action = Tick Int | SetStart Cell
 
 main :: IO ()
-main = mainWidget view
+main = mainWidget $ do
+    rec changes <- view model
+        model <- foldDyn update (initModel 8 8) changes
+    return ()
 
+update :: Action -> Model -> Model
+update action m@(Model r c p b) =
+    case action of
+        SetStart start -> Model r c [start] b
+        Tick t ->  Model r c p b
+            
 ns = Just "http://www.w3.org/2000/svg"
 
--- view :: MonadWidget t m => m (Event t Action)
-view :: MonadWidget t m => m ()
-view = do
+view :: MonadWidget t m => Dynamic t Model -> m (Event t Action)
+-- view :: MonadWidget t m => m ()
+view m = do
     let 
+        fillColor r c = if (r + c) `mod` 2 == 0 then "blue" else "grey"
+
         showChecker :: MonadWidget t m => Int -> Int -> m (Event t Action)
         showChecker r c = do
-            (el, _) <- elDynAttrNS' ns "rect" (constDyn $ "x" =: show c <> "y" =: show r <> "width" =: "1" <> "height" =: "1" <> "fill" =: if (r + c) `mod` 2 == 0 then "blue" else "grey") $ return ()
+            (el, _) <- elDynAttrNS' ns "rect" 
+                           (constDyn $  "x" =: show c 
+                                     <> "y" =: show r 
+                                     <> "width" =: "1" 
+                                     <> "height" =: "1" 
+                                     <> "fill" =: fillColor r c)
+                       $ return ()
             return $ const (SetStart (r,c)) <$> domEvent Click el -- tricky
 
         showCheckers = 
@@ -38,8 +57,10 @@ view = do
                        [showChecker r c]
             in sequence ckrs -- tricky
     
-    
-    elDynAttrNS' ns "svg" (constDyn $ "viewBox" =: "0 0 8 8" <>  "width" =: "500" <> "height" =: "500") $ 
-        elStopPropagationNS ns "g" Click (fmap leftmost showCheckers)
-    return ()
+    (el, ev) <- elDynAttrNS' ns "svg" 
+                    (constDyn $  "viewBox" =: "0 0 8 8" 
+                              <> "width" =: "500"
+                              <> "height" =: "500") 
+                $ elStopPropagationNS ns "g" Click (fmap leftmost showCheckers)
+    return ev
 
