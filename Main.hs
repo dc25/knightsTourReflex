36bd69bbce913322a1b376da5698955d290dd238
Dave Compton <davecompton7@gmail.com>
2016-04-22 21:18:48 +0000

    introducing code for showing moves but not using it yet.

--- Main.hs
+++ Main.hs
@@ -19,8 +19,9 @@ data Model = Model { rows :: Int
 initModel rc cc = 
     let board = do r <- [0..rc-1] 
                    c <- [0..cc-1] 
-                   return (r,c)
-    in Model rc cc [] board
+                   [(r,c)]
+        path = []
+    in Model rc cc path board
 
 data Action = Tick | SetStart Cell
 
@@ -30,8 +31,7 @@ main = do
     mainWidget $ do
         rec changes <- view model
             ticks <- fmap (const Tick) <$> tickLossy 1 startTime 
-            let actions = mergeWith const [changes, ticks]
-            model <- foldDyn update (initModel 8 8) actions
+            model <- foldDyn update (initModel 8 8) $ mergeWith const [changes, ticks]
         return ()
 
 nextMoves :: Model -> Cell -> [Cell]
@@ -43,11 +43,15 @@ nextMoves model@(Model r c p b) startCell =
               if abs cx == abs cy then [] else [(cx,cy)]
 
       jumps = map (\cell -> (fst cell + fst startCell, snd cell + snd startCell)) km
+
   in filter (\j -> elem j b && notElem j p ) jumps
 
-bestMove :: Model -> Cell
+bestMove :: Model -> Maybe Cell
 bestMove model@(Model _ _ p _) = 
-    minimumBy (compare `on` (length . nextMoves model)) (nextMoves model $ head p)
+    let options = (nextMoves model $ head p)
+    in if null options 
+       then Nothing 
+       else Just $ minimumBy (compare `on` (length . nextMoves model)) options
 
 update :: Action -> Model -> Model
 update action m@(Model r c p b) =
@@ -55,15 +59,15 @@ update action m@(Model r c p b) =
         SetStart start -> 
             Model r c [start] b
         Tick ->  
-            if null p then 
-                m
-            else
-                Model r c (bestMove m:p) b
+            if null p then m 
+            else case bestMove m of
+                     Nothing -> m
+                     Just best ->  Model r c (best:p) b
             
 ns = Just "http://www.w3.org/2000/svg"
 
 view :: MonadWidget t m => Dynamic t Model -> m (Event t Action)
-view m = do
+view model = do
     let 
         fillColor r c = if (r + c) `mod` 2 == 0 then "blue" else "grey"
 
@@ -78,16 +82,29 @@ view m = do
                        $ return ()
             return $ const (SetStart (r,c)) <$> domEvent Click el -- tricky
 
-        showCheckers = 
-            let ckrs = do r <- [0..7] 
-                          c <- [0..7] 
-                          return $ showChecker r c
+        checkers r c = 
+            let ckrs = do row <- [0..r-1] 
+                          col <- [0..c-1] 
+                          [showChecker row col]
             in sequence ckrs -- tricky
+
+        showMove :: MonadWidget t m => Cell -> Cell -> m (Event t Action)
+        showMove pt0 pt1 = do
+            elDynAttrNS' ns "line" 
+                 (constDyn $  "x1" =: show ((snd pt0 & fromIntegral :: Float) + 0.5)
+                           <> "y1" =: show ((fst pt0 & fromIntegral :: Float) + 0.5)
+                           <> "x2" =: show ((snd pt1 & fromIntegral :: Float) + 0.5)
+                           <> "y2" =: show ((fst pt1 & fromIntegral :: Float) + 0.5)
+                           <> "style" =: "stroke:yellow;stroke-width:0.05" ) 
+                 $ return ()
+            return never
+
+        moves (Model _ _ p _) = 
+            if null p then [] else zipWith showMove p $ tail p
     
     (el, ev) <- elDynAttrNS' ns "svg" 
                     (constDyn $  "viewBox" =: "0 0 8 8" 
                               <> "width" =: "500"
                               <> "height" =: "500") 
-                $ elStopPropagationNS ns "g" Click (fmap leftmost showCheckers)
+                $ elStopPropagationNS ns "g" Click (leftmost <$> checkers 8 8)
     return ev
-
